363642809387016b7e41c2439310d0d8d31e4337
Adrian <66483092+serbinskis@users.noreply.github.com>
2026-08-15 03:39:53 +0300

    fixes

--- MiniRecycleBin/MiniRecycleBin.dpr
+++ MiniRecycleBin/MiniRecycleBin.dpr
@@ -1,6 +1,6 @@
-program MiniRecycleBin_Created_By_Serbinskis;
+program MiniRecycleBin;
 
-//{$APPTYPE CONSOLE}
+// {$APPTYPE CONSOLE}
 
 {$R MiniRecycleBin.res}
 
@@ -33,9 +33,8 @@ type
     i64NumItems: Int64;
   end;
 
-type
-  TSHQueryRecycleBin = function(RootPath: PChar; Query: PSHQueryRBInfo): Integer; stdcall;
-  TSHEmptyRecycleBin = function(Wnd: HWND; pszRootPath: PChar; dwFlags: DWORD): HRESULT; stdcall;
+function SHQueryRecycleBin(RootPath: PChar; Query: PSHQueryRBInfo): HRESULT; stdcall; external 'shell32.dll' name 'SHQueryRecycleBinA';
+function SHEmptyRecycleBin(Wnd: HWND; pszRootPath: PChar; dwFlags: DWORD): HRESULT; stdcall; external 'shell32.dll' name 'SHEmptyRecycleBinA';
 
 const
   UPDATE_TIMEOUT = 1500;
@@ -47,14 +46,13 @@ var
   TaskbarRestart: Cardinal;
   WM_CALLBACK_MESSAGE: Integer;
   hMainHandle: HWND;
-  LibHandle, hMutex: THandle;
+  ulNotifyHandle: ULONG = 0; // Global handle for Shell Notify
+  hMutex: THandle;
   EventHandler: TEventHandler;
   LWndClass: TWndClass;
 
 var
-  SHQueryRecycleBin: TSHQueryRecycleBin;
   SHQueryRBInfo: TSHQueryRBInfo;
-  SHEmptyRecycleBin: TSHEmptyRecycleBin;
 
 var
   NotifyEvents: TShellNotifyEvents;
@@ -75,69 +73,76 @@ var
   i: Integer;
 
 
-//FormatSize
+// FormatSize
 function FormatSize(x: Int64): String;
 begin
   Result := Format('%d Bytes', [x]);
-  if x > (1024) then Result := Format('%d KB', [Round(x / 1024)]);
-  if x > (1024 * 1024) then Result := Format('%d MB', [Round(x / (1024 * 1024))]);
+  if x > (1024) then Result := Format('%d KB', [x div 1024]);
+  if x > (1024 * 1024) then Result := Format('%d MB', [x div (1024 * 1024)]);
   if x > (1024 * 1024 * 1024) then Result := Format('%.2f GB', [x / (1024 * 1024 * 1024)]);
 end;
-//FormatSize
+// FormatSize
 
 
-//OutputConsole
+// OutputConsole
 procedure OutputConsole(S: String);
 begin
   if (GetStdHandle(STD_OUTPUT_HANDLE) <> 0) then WriteLn(S);
 end;
-//OutputConsole
+// OutputConsole
 
 
-//UpdateRecycleBin
+// UpdateRecycleBin
 procedure UpdateRecycleBin;
 begin
-  if MillisecondsBetween(SavedTime, Now) < UPDATE_TIMEOUT then Exit;
+  if (MillisecondsBetween(SavedTime, Now) < UPDATE_TIMEOUT) then Exit;
   SavedTime := Now;
 
-  OutputConsole('UpdateRecycleBin');
-  EventHandler.OnTimer(nil);
-  Timer.Interval := UPDATE_TIMEOUT;
+  OutputConsole('UpdateRecycleBin -> Triggering status update...');
+  Timer.Enabled := False;
+  Timer.Interval := 50;
   Timer.Enabled := True;
 end;
-//UpdateRecycleBin
+// UpdateRecycleBin
 
 
-//RegisterShellNotify
+// RegisterShellNotify
 procedure RegisterShellNotify;
 begin
-  OutputConsole('RegisterShellNotify');
+  OutputConsole('RegisterShellNotify -> Registering file system change hooks...');
 
-  //Create notify event
+  // Deregister old notification handle if it exists
+  if (ulNotifyHandle <> 0) then begin
+    SHChangeNotifyDeregister(ulNotifyHandle);
+    ulNotifyHandle := 0;
+  end;
+
+  // Create notify event
   NotifyEvents := [neCreate, neDelete, neMkDir, neRmDir, neUpdateDir, neUpdateItem];
   Events := NotifyEventsToLongint(NotifyEvents);
 
-  //Fill information
+  // Fill information
   Entries := 1;
   NotifyEntrys[0].pidl := nil;
   NotifyEntrys[0].fRecursive := True;
 
-  //Register notify change
-  SHChangeNotifyRegister(hMainHandle, SHCNF_ACCEPT_INTERRUPTS + SHCNF_ACCEPT_NON_INTERRUPTS, Events, WM_SHELLNOTIFY, Entries, @NotifyEntrys);
+  // Register notify change
+  ulNotifyHandle := SHChangeNotifyRegister(hMainHandle, SHCNF_ACCEPT_INTERRUPTS + SHCNF_ACCEPT_NON_INTERRUPTS, Events, WM_SHELLNOTIFY, Entries, @NotifyEntrys);
 end;
-//RegisterShellNotify
+// RegisterShellNotify
 
 
-//Process timer events
+// Process timer events
 procedure TEventHandler.OnTimer(Sender: TObject);
 begin
+  Timer.Interval := UPDATE_TIMEOUT;
   Timer.Enabled := False;
-  OutputConsole('Timer event');
+  OutputConsole('Timer event -> Updating Recycle Bin Data...');
 
   SHQueryRecycleBin(nil, @SHQueryRBInfo);
   StrPCopy(TrayIconData.szTip, FormatSize(SHQueryRBInfo.i64Size));
 
-  if SHQueryRBInfo.i64NumItems <> 0 then begin
+  if (SHQueryRBInfo.i64NumItems <> 0) then begin
     TrayIconData.hIcon := LoadIcon(HInstance, 'RECYCLEFULL');
     PopupMenu.Items[1].Enabled := True;
   end else begin
@@ -153,34 +158,35 @@ begin
     Shell_NotifyIcon(NIM_MODIFY, @TrayIconData);
   end;
 end;
-//Process timer events
+// Process timer events
 
 
-//Process popup menu events
+// Process popup menu events
 procedure TEventHandler.HandlePopupItem(Sender: TObject);
 begin
-  if TMenuItem(Sender).Caption = 'Open' then begin
+  if (TMenuItem(Sender).Caption = 'Open') then begin
     ShellExecute(0, 'open', PChar('shell:RecycleBinFolder'), nil, nil, SW_SHOW);
   end;
 
-  if TMenuItem(Sender).Caption = 'Empty' then begin
+  if (TMenuItem(Sender).Caption = 'Empty') then begin
     SHEmptyRecycleBin(0, nil, 0);
     OnTimer(nil);
   end;
 
-  if TMenuItem(Sender).Caption = 'Update' then begin
+  if (TMenuItem(Sender).Caption = 'Update') then begin
     OnTimer(nil);
   end;
 
-  if TMenuItem(Sender).Caption = 'Exit' then begin
+  if (TMenuItem(Sender).Caption = 'Exit') then begin
     Shell_NotifyIcon(NIM_DELETE, @TrayIconData);
+    if (ulNotifyHandle <> 0) then SHChangeNotifyDeregister(ulNotifyHandle);
     TerminateProcess(OpenProcess(PROCESS_TERMINATE, False, GetCurrentProcessId), 0);
   end;
 end;
-//Process popup menu events
+// Process popup menu events
 
 
-//Process tray icon messages
+// Process tray icon messages
 function WindowProc(hWnd, Msg: Longint; wParam: WPARAM; lParam: LPARAM): Longint; stdcall;
 var
   Point: TPoint;
@@ -190,24 +196,27 @@ var
 begin
   OutputConsole('Message -> ' + IntToStr(Msg) + ' | lParam -> ' + IntToStr(lParam) + ' | wParam -> ' + IntToStr(wParam));
 
-  if Msg = WM_SHELLNOTIFY then begin
+  if (Msg = WM_SHELLNOTIFY) then begin
     NotifyEvent := LongintToNotifyEvent(LParam and SHCNE_ALLEVENTS);
     pItem := PItemIDArray(wParam);
-    Path := PidlToStr(pItem^.pidl[0]);
+    // SAFETY CHECK: Ensure pointers are not NIL
+    if (pItem <> nil) and (pItem^.pidl[0] <> nil) then begin
+      Path := PidlToStr(pItem^.pidl[0]);
 
-    case NotifyEvent of
-      neCreate: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neDelete: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neMkDir: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neRenameFolder: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neRenameItem: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neRmDir: if Pos(RECYCLE_BIN, UpperCase(Path)) > 0 then UpdateRecycleBin;
-      neUpdateDir: UpdateRecycleBin;
-      neUpdateItem: UpdateRecycleBin;
+      case NotifyEvent of
+        neCreate: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neDelete: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neMkDir: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neRenameFolder: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neRenameItem: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neRmDir: if (Pos(RECYCLE_BIN, UpperCase(Path)) > 0) then UpdateRecycleBin;
+        neUpdateDir: UpdateRecycleBin;
+        neUpdateItem: UpdateRecycleBin;
+      end;
     end;
   end;
 
-  if Msg = WM_CALLBACK_MESSAGE then begin
+  if (Msg = WM_CALLBACK_MESSAGE) then begin
     case lParam of
       WM_RBUTTONUP: begin
         GetCursorPos(Point);
@@ -221,48 +230,43 @@ begin
     end;
   end;
 
-  if Msg = TaskbarRestart then begin
+  if (Msg = TaskbarRestart) then begin
     ExplorerRestarted := True;
     EventHandler.OnTimer(nil);
   end;
 
   Result := DefWindowProc(hWnd, Msg, wParam, lParam);
 end;
-//Process tray icon messages
+// Process tray icon messages
 
 
-//Main
+// Main
 begin
-  //Check if application is already running
+  // Check if application is already running
   hMutex := CreateMutex(nil, False, APP_NAME);
-  if WaitForSingleObject(hMutex, 0) = WAIT_TIMEOUT then begin
+  if (WaitForSingleObject(hMutex, 0) = WAIT_TIMEOUT) then begin
     Application.Title := 'Mini Recycle Bin';
     ShowMessage('Mini Recycle Bin is already running.');
     Exit;
   end;
 
-  //Generate random callback message
+  // Generate random callback message
   Randomize;
-  WM_CALLBACK_MESSAGE := WM_USER + Random(131072);
-
-  //Load lib for recycle bin functions
-  LibHandle := SafeLoadLibrary('shell32.dll');
-  @SHQueryRecycleBin := GetProcAddress(LibHandle, 'SHQueryRecycleBinA');
-  @SHEmptyRecycleBin := GetProcAddress(LibHandle, 'SHEmptyRecycleBinA');
+  WM_CALLBACK_MESSAGE := WM_USER + 100 + Random(20000);
   SHQueryRBInfo.cbSize := SizeOf(SHQueryRBInfo);
 
-  //Create class
+  // Create class
   LWndClass.hInstance := HInstance;
   LWndClass.lpszClassName := APP_NAME + 'Wnd';
   LWndClass.Style := CS_PARENTDC;
   LWndClass.lpfnWndProc := @WindowProc;
 
-  //Register class and create window and get taskbar restart message
+  // Register class and create window and get taskbar restart message
   Windows.RegisterClass(LWndClass);
   hMainHandle := CreateWindow(LWndClass.lpszClassName, APP_NAME, 0,0,0,0,0,0,0, HInstance, nil);
   TaskbarRestart := RegisterWindowMessage('TaskbarCreated');
 
-  //Create tray icon
+  // Create tray icon
   FillChar(TrayIconData, SizeOf(TrayIconData), 0);
   TrayIconData.cbSize := SizeOf(TrayIconData);
   TrayIconData.Wnd := hMainHandle;
@@ -270,12 +274,12 @@ begin
   TrayIconData.uCallbackMessage := WM_CALLBACK_MESSAGE; //User definied messege
   TrayIconData.hIcon := LoadIcon(HInstance, 'RECYCLEEMPTY');
 
-  //Create popup menu
+  // Create popup menu
   PopupMenu := TPopupMenu.Create(nil);
   PopupMenu.AutoHotkeys := maManual;
   EventHandler := TEventHandler.Create;
 
-  //Add items to popup menu
+  // Add items to popup menu
   for i := 0 to Length(MenuItems)-1 do begin
     Item := TMenuItem.Create(PopupMenu);
     Item.Caption := MenuItems[i];
@@ -283,17 +287,17 @@ begin
     PopupMenu.Items.Add(Item);
   end;
 
-  //Create timer
+  // Create timer
   Timer := TTimer.Create(nil);
   Timer.OnTimer := EventHandler.OnTimer;
   Timer.Enabled := False;
 
-  //Call this event because it will add tray icon & register shell notify
+  // Call this event because it will add tray icon & register shell notify
   ExplorerRestarted := True;
   EventHandler.OnTimer(nil);
 
-  //Process messages and loop program
-  while GetMessage(Msg, 0,0,0) do begin
+  // Process messages and loop program
+  while GetMessage(Msg, 0, 0, 0) do begin
     TranslateMessage(Msg);
     DispatchMessage(Msg);
   end;
